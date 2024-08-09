362ab4e808a4d06c744a22ba508645e1fd0a8fa0
DexterPac <144158622+DexterPac@users.noreply.github.com>
2024-08-08 17:40:09 -0700

    Delete Final Project directory

--- Final Project/indexcode.js
+++
@@ -1,5 +0,0 @@
-//console.log(window.performance);
-var loadduration = performance.timing.responseEnd - performance.timing.navigationStart; //in milliseconds
-//console.log(loadduration)
-
-document.getElementById("footertext").innerText += " " + loadduration/1000 + " SECONDS";
